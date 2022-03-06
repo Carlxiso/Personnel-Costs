7fbdc02840b93d3500fb69bc012b92673e11e109
carlxiso <lavaredasnarciso@gmail.com>
2022-03-06 11:49:25 +0000

    Widget ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -17,63 +17,74 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      /*The SingleChildScrollView is wrapped with a container so we can 
-      have the list into a spave of 400px*/
-      child: SingleChildScrollView(
-        child: Column(
-          /* A função map vai receber uma outro função, 
-                que por sua vez essa mesma função vai receber 
-                cada um dos elementos*/
-          /* A ideia é converter um objecto do tipo transaction 
-                para um componente visual*/
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
+      /*The ListView Widget will do the same thing we did with 
+      the SingleChildScrollView widget
+
+      just like the SingleChildScrollView this component needs to have 
+      a parent component with a pre-defined size
+      */
+
+      child: ListView.builder(
+        /*
+        With the ListView.builder you can control the number of items 
+        displayed. This means that the number of rendering is controlled 
+        or it makes the application faster 
+        */
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    '€ ${tr.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                       color: Colors.purple,
-                      width: 2,
-                    )),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '€ ${tr.value.toStringAsFixed(2)}',
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tr.title,
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
+                        fontSize: 16,
+                        color: Colors.black,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: const TextStyle(
+                        color: Colors.grey,
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        /* A função map vai receber uma outro função, 
+              que por sua vez essa mesma função vai receber 
+              cada um dos elementos*/
+        /* A ideia é converter um objecto do tipo transaction 
+              para um componente visual Que neste cado será o Card Widget*/
+        // children: transactions.map((tr) {}).toList(),
       ),
     );
   }
